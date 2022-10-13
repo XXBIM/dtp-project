50cd8f99d6838dbc07069e80ec52f56dd7363b82
XXBIM <akabimgaming@gmail.com>
2022-10-13 12:40:48 +0700

    woakowka

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -26,21 +26,21 @@
         <div class="text-center mb-4">
           <a href="." class="navbar-brand navbar-brand-autodark"><img src="./static/logo.svg" height="36" alt=""></a>
         </div>
-        <form class="card card-md" ethod="POST" action="{{ route('register') }}">
+        <form class="card card-md" method="POST" action="{{ route('register') }}">
           <div class="card-body">
             <h2 class="card-title text-center mb-4">Create new account</h2>
             <div class="mb-3">
-              <label class="form-label">Name</label>
-              <input type="text" class="form-control" placeholder="Enter name">
+              <label class="form-label" for="name" :value="__('Name')">Name</label>
+              <input type="text" name="name" class="form-control" placeholder="Enter name" :value="old('name')" required autofocus>
             </div>
             <div class="mb-3">
-              <label class="form-label">Email address</label>
-              <input type="email" name="email" class="form-control" placeholder="Enter email">
+              <label class="form-label" for="email" :value="__('Email')">Email address</label>
+              <input type="email" name="email" class="form-control" placeholder="Enter email" :value="old('email')" required>
             </div>
             <div class="mb-3">
-              <label class="form-label">Password</label>
+              <label class="form-label" for="password" :value="__('Password')">Password</label>
               <div class="input-group input-group-flat">
-                <input type="password" name="password" class="form-control"  placeholder="Password"  autocomplete="off">
+                <input type="password" name="password" class="form-control"  placeholder="Password" required autocomplete="new-password">
                 <span class="input-group-text">
                   <a href="#" class="link-secondary" title="Show password" data-bs-toggle="tooltip"><!-- Download SVG icon from http://tabler-icons.io/i/eye -->
                     <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><circle cx="12" cy="12" r="2" /><path d="M22 12c-2.667 4.667 -6 7 -10 7s-7.333 -2.333 -10 -7c2.667 -4.667 6 -7 10 -7s7.333 2.333 10 7" /></svg>
