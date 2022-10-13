9b3c608abbb14debb4e186ad0506da6e73c3311c
XXBIM <akabimgaming@gmail.com>
2022-10-13 12:24:54 +0700

    wokaowkao

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -37,12 +37,12 @@
             </div>
             <div class="mb-3">
               <label class="form-label">Email address</label>
-              <input type="email" class="form-control" placeholder="Enter email">
+              <input type="name" class="form-control" placeholder="Enter email">
             </div>
             <div class="mb-3">
               <label class="form-label">Password</label>
               <div class="input-group input-group-flat">
-                <input type="password" class="form-control"  placeholder="Password"  autocomplete="off">
+                <input type="name" class="form-control"  placeholder="Password"  autocomplete="off">
                 <span class="input-group-text">
                   <a href="#" class="link-secondary" title="Show password" data-bs-toggle="tooltip"><!-- Download SVG icon from http://tabler-icons.io/i/eye -->
                     <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><circle cx="12" cy="12" r="2" /><path d="M22 12c-2.667 4.667 -6 7 -10 7s-7.333 -2.333 -10 -7c2.667 -4.667 6 -7 10 -7s7.333 2.333 10 7" /></svg>
